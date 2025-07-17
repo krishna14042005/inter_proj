3dca989140c2680c906077d84d1b2c0bfbda01ab
krishna14042005 <krishnapradhap.g2023lcse@sece.ac.in>
2025-07-17 14:06:39 +0530

    updatae

--- app/page.js
+++ app/page.js
@@ -2,6 +2,7 @@
 import styles from "./page.module.css";
 import {signIn, useSession, signOut} from "next-auth/react"
 import './globals.css'
+import Navbar from "@/component/Navbar";
 
 export default function Home() {
   const{data:session}=useSession();
@@ -9,7 +10,7 @@ export default function Home() {
   if(session){
    return(
     <>
-    <p>You are not signed in as a user{session.user.email}</p>
+    <p>You are signed in as a user{session.user.email}</p>
    
     <button onClick={()=>signOut()}>Sign Out</button>
     </>
